203be276f169e82a5e196a16f096adf348b2e8f7
Josh Portner <joshua.p@gmail.com>
2013-07-10 20:06:45 -0700

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buff = fs.readFileSync('index.html');
+var content = buff.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
